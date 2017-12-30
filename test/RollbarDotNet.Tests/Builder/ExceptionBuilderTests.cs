7cf3990cfe5bed2e41f73a4e4062de3737cbb13f
William Roush <william.roush@roushtech.net>
2017-12-30 16:55:18 -0500

    Some work being done on dynamic code exceptions.

--- test/RollbarDotNet.Tests/Builder/ExceptionBuilderTests.cs
+++ test/RollbarDotNet.Tests/Builder/ExceptionBuilderTests.cs
@@ -4,9 +4,15 @@
     using RollbarDotNet.Builder;
     using System;
     using System.Diagnostics;
+    using System.IO;
     using System.Linq;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using System.Reflection.Metadata;
+    using System.Reflection.Metadata.Ecma335;
+    using System.Reflection.PortableExecutable;
     using Xunit;
-
+    
     public class ExceptionBuilderTests
     {
         public ExceptionBuilderTests()
@@ -18,6 +24,8 @@
         protected ExceptionBuilder ExceptionBuilder { get; set; }
 
         protected Payload Payload { get; set; }
+
+        protected delegate void ExceptionDelegate(string s1, int i1);
 
         [Fact]
         public void SetsPayload()
@@ -31,18 +39,52 @@
                 this.ExceptionBuilder.Execute(this.Payload, exception);
                 var payload = this.Payload.Data?.Body?.TraceChain?.FirstOrDefault();
                 Assert.Equal("Exception", payload?.Exception?.Class);
-                Assert.Equal("test exception", payload?.Exception ?.Message);
+                Assert.Equal("test exception", payload?.Exception?.Message);
                 Assert.True(payload?.Frames?.Count == 2);
-                var frame = payload?.Frames?.FirstOrDefault();
-                Assert.Equal("RollbarDotNet.Tests.Builder.ExceptionBuilderTests.ThrowException(System.String a, System.Int32 b)", frame?.Method);
+                var frame = payload.Frames?.FirstOrDefault();
+                Assert.Equal(
+                    "RollbarDotNet.Tests.Builder.ExceptionBuilderTests.ThrowException(System.String a, System.Int32 b)",
+                    frame?.Method);
                 var stackTrace = new StackTrace(exception, true);
                 var stackTraceFrame = stackTrace.GetFrames().FirstOrDefault();
+                Assert.NotNull(stackTraceFrame);
                 Assert.Equal(stackTraceFrame.GetFileColumnNumber(), frame?.ColumnNumber);
                 Assert.Equal(stackTraceFrame.GetFileLineNumber(), frame?.LineNumber);
                 Assert.Equal(stackTraceFrame.GetFileName(), frame?.Filename);
             }
         }
 
+        [Fact]
+        public void HandlesDynamicMethods()
+        {
+            byte[] ilBytes;
+            var methodInfo = typeof(ExceptionBuilderTests).GetMethod("ThrowException");
+            var metadataToken = methodInfo.GetMetadataToken();
+            using (var stream = File.OpenRead(methodInfo.DeclaringType.GetTypeInfo().Assembly.Location))
+            using (var peReader = new PEReader(stream))
+            {
+                var metadataReader = peReader.GetMetadataReader();
+                var methodHandle = MetadataTokens.MethodDefinitionHandle(metadataToken);
+                var methodDef = metadataReader.GetMethodDefinition(methodHandle);
+                var methodBody = peReader.GetMethodBody(methodDef.RelativeVirtualAddress);
+                ilBytes = methodBody.GetILBytes();
+            }
+            
+            
+
+            var dynamicMethod = new DynamicMethod("ThrowException", null, new[] { typeof(string), typeof(string) },
+                typeof(ExceptionBuilderTests).GetTypeInfo().Module);
+            var dynamicMethodDelegate = (ExceptionDelegate)dynamicMethod.CreateDelegate(typeof(ExceptionDelegate));
+            try
+            {
+                dynamicMethodDelegate(string.Empty, 0);
+            }
+            catch (System.Exception exception)
+            {
+                this.ExceptionBuilder.Execute(this.Payload, exception);
+            }
+        }
+        
         protected void ThrowException(string a, int b)
         {
             throw new System.Exception("test exception");
